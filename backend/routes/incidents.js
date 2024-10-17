3a88ca0fee204e041e2cf04e92edd6487423f23c
Kevin Lam <kevin.k.f.lam1@gmail.com>
2024-10-17 11:03:51 -0700

    post request to add to mongodb works now

--- backend/routes/incidents.js
+++ backend/routes/incidents.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Incident = require('../models/incidentModel');
 
 const router = express.Router();
 
@@ -6,8 +7,25 @@ router.get('/', (req, res) => {
   res.json({ msg: 'GETTING ALL INCIDENTS' });
 });
 
-router.post('/', (req, res) => {
-  res.json({ msg: 'POST AN INCIDENT' });
+router.post('/', async (req, res) => {
+  const { datetime, type_of_incident, ski_run, skier_age, description } =
+    req.body;
+
+  try {
+    const incident = await Incident.create({
+      datetime,
+      type_of_incident,
+      ski_run: {
+        name: ski_run.name,
+        difficulty: ski_run.difficulty,
+      },
+      skier_age,
+      description,
+    });
+    res.status(200).json(incident);
+  } catch (e) {
+    res.json({ e: 'Failed to POST a new incident' });
+  }
 });
 
 module.exports = router;
