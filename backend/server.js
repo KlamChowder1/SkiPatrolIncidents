ed2f87124b6cd69182a80d01a188f94d04d38b7f
Kevin Lam <kevin.k.f.lam1@gmail.com>
2024-10-17 10:30:14 -0700

    added routes and fixed env fil

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
+const incidentRoutes = require('./routes/incidents');
 
 const app = express();
+
+// some middleware to parse requests into req.body
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use('/api/incidents', incidentRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log('listening on port 4000');
