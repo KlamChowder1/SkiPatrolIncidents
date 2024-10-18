256a24fc20c605e3f55e05ff13c435eca01f0766
Kevin Lam <kevin.k.f.lam1@gmail.com>
2024-10-17 22:28:21 -0700

    fixing submit form to match post request

--- ski-patrol-incidents/src/components/AddIncidentForm.jsx
+++ ski-patrol-incidents/src/components/AddIncidentForm.jsx
@@ -7,26 +7,70 @@ import {
   Box,
   Alert,
   Snackbar,
+  MenuItem,
+  FormControl,
 } from '@mui/material';
 
 export default function AddIncidentForm() {
+  // TODO:check if I should camelCase or snake_case this
   const [formData, setFormData] = useState({
-    date: '',
-    type: '',
-    location: '',
+    datetime: '',
+    type_of_incident: '',
+    ski_run: {
+      name: '',
+      difficulty: '',
+    },
+    skier_age: '',
     description: '',
   });
   const [openSnackbar, setOpenSnackbar] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+
+    // Handling nested ski_run field separately
+    if (name === 'ski_run_name' || name === 'ski_run_difficulty') {
+      setFormData({
+        ...formData,
+        ski_run: {
+          ...formData.ski_run,
+          [name === 'ski_run_name' ? 'name' : 'difficulty']: value,
+        },
+      });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Incident Data: ', formData);
-    setOpenSnackbar(true);
+    try {
+      const response = await fetch('/api/incidents', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        console.log('POSTED OKAY');
+        setFormData({
+          datetime: '',
+          type_of_incident: '',
+          ski_run: {
+            name: '',
+            difficulty: '',
+          },
+          skier_age: '',
+          description: '',
+        });
+        setOpenSnackbar(true);
+      } else {
+        console.error('ERROR');
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -35,12 +79,12 @@ export default function AddIncidentForm() {
         <Typography variant="h4" gutterBottom>
           Add Ski Incident
         </Typography>
-        <form onSubmit={handleSubmit}>
+        <FormControl fullWidth>
           <TextField
             label="Date"
-            type="date"
-            name="date"
-            value={formData.date}
+            type="datetime-local"
+            name="datetime"
+            value={formData.datetime}
             onChange={handleChange}
             fullWidth
             InputLabelProps={{ shrink: true }}
@@ -48,17 +92,41 @@ export default function AddIncidentForm() {
           />
           <TextField
             label="Type of Incident"
-            name="type"
-            value={formData.type}
+            name="type_of_incident"
+            value={formData.type_of_incident}
             onChange={handleChange}
             fullWidth
             margin="normal"
             required
           />
           <TextField
-            label="Location (Run/Area)"
-            name="location"
-            value={formData.location}
+            label="Location (Ski Run Name)"
+            name="ski_run_name"
+            value={formData.ski_run.name}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+          />
+          <TextField
+            label="Ski Run Difficulty"
+            name="ski_run_difficulty"
+            value={formData.ski_run.difficulty}
+            onChange={handleChange}
+            fullWidth
+            margin="normal"
+            required
+            select
+          >
+            <MenuItem value="Green">Green</MenuItem>
+            <MenuItem value="Blue">Blue</MenuItem>
+            <MenuItem value="Black Diamond">Black Diamond</MenuItem>
+          </TextField>
+          <TextField
+            label="Skier Age"
+            name="skier_age"
+            type="number"
+            value={formData.skier_age}
             onChange={handleChange}
             fullWidth
             margin="normal"
@@ -81,12 +149,12 @@ export default function AddIncidentForm() {
             color="primary"
             sx={{ mt: 2 }}
             fullWidth
+            onClick={handleSubmit}
           >
             Submit Incident
           </Button>
-        </form>
+        </FormControl>
       </Box>
-      {/* TODO: make Snackbar in a share component?  */}
       <Snackbar
         open={openSnackbar}
         autoHideDuration={2000}
